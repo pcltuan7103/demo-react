8c1f2866ad7a4f671389c2f1aea19bd2fddda0fd
tuanpcl7103 <tuanpcl7103.gmail.com>
2024-08-03 20:14:07 +0700

    Design Admin SideBar

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,5 +1,25 @@
+import SideBar from "./SideBar";
+import "./Admin.scss";
+import { useState } from "react";
+import { FaBars } from "react-icons/fa";
+
 const Admin = (props) => {
-  return <div>Admin Component</div>;
+  const [collapsed, setCollaped] = useState(false);
+
+  return (
+    <div className="admin-container">
+      <div className="admin-sidebar">
+        <SideBar collapsed={collapsed} />
+      </div>
+      <div className="admin-content">
+        <FaBars
+          onClick={() => {
+            setCollaped(!collapsed);
+          }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Admin;
