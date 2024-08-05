849c22f6ee6d7cf3b91ea798ad54ac9cebe4d187
tuanpcl7103 <tuanpcl7103.gmail.com>
2024-08-06 01:48:18 +0700

    Finish Update User

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -4,11 +4,16 @@ import { FcPlus } from "react-icons/fc";
 import TableUser from "./TableUser";
 import { useEffect, useState } from "react";
 import { getAllUsers } from "../../../services/apiServices";
+import ModalUpdateUser from "./ModalUpdateUser";
 
 const ManageUser = (props) => {
   const [showModalCreateUser, setShowModalCreateUser] = useState(false);
 
+  const [showModalUpdateUser, setShowModalUpdateUser] = useState(false);
+
   const [listUsers, setListUsers] = useState([]);
+
+  const [dataUpdate, setDataUpdate] = useState({});
 
   useEffect(() => {
     fecthListUsers();
@@ -19,6 +24,15 @@ const ManageUser = (props) => {
     if (res.EC === 0) {
       setListUsers(res.DT);
     }
+  };
+
+  const handleClickBtnUpdate = (user) => {
+    setShowModalUpdateUser(true);
+    setDataUpdate(user);
+  };
+
+  const resetUpdateData = () => {
+    setDataUpdate({});
   };
 
   return (
@@ -34,12 +48,23 @@ const ManageUser = (props) => {
           </button>
         </div>
         <div className="table-users-content">
-          <TableUser listUsers={listUsers} />
+          <TableUser
+            listUsers={listUsers}
+            handleClickBtnUpdate={handleClickBtnUpdate}
+          />
         </div>
         <ModalCreateUser
           show={showModalCreateUser}
           setShow={setShowModalCreateUser}
           fecthListUsers={fecthListUsers}
+        />
+
+        <ModalUpdateUser
+          show={showModalUpdateUser}
+          setShow={setShowModalUpdateUser}
+          dataUpdate={dataUpdate}
+          fecthListUsers={fecthListUsers}
+          resetUpdateData={resetUpdateData}
         />
       </div>
     </div>
--- src/components/Admin/Content/TableUser.js
+++ src/components/Admin/Content/TableUser.js
@@ -25,7 +25,12 @@ const TableUser = (props) => {
                   <td>{user.role}</td>
                   <td>
                     <button className="btn btn-secondary">View</button>
-                    <button className="btn btn-warning mx-3">Update</button>
+                    <button
+                      className="btn btn-warning mx-3"
+                      onClick={() => props.handleClickBtnUpdate(user)}
+                    >
+                      Update
+                    </button>
                     <button className="btn btn-danger">Delete</button>
                   </td>
                 </tr>
