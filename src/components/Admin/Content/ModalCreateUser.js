c86de9dedc3a31e60a52544707394c76471f9262
tuanpcl7103 <tuanpcl7103.gmail.com>
2024-08-05 21:36:07 +0700

    Customize Axios

--- src/components/Admin/Content/ModalCreateUser.js
+++ src/components/Admin/Content/ModalCreateUser.js
@@ -5,6 +5,7 @@ import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import { FcPlus } from "react-icons/fc";
 import { toast } from "react-toastify";
+import { postAddNewUser } from "../../../services/apiServices";
 
 function ModalCreateUser(props) {
   const { show, setShow } = props;
@@ -56,25 +57,15 @@ function ModalCreateUser(props) {
       return;
     }
 
-    const data = new FormData();
-    data.append("email", email);
-    data.append("password", password);
-    data.append("username", username);
-    data.append("role", role);
-    data.append("userImage", image);
+    let data = await postAddNewUser(email, password, username, role, image);
 
-    let res = await axios.post(
-      "http://localhost:8081/api/v1/participant",
-      data
-    );
-
-    if (res.data && res.data.EC === 0) {
-      toast.success(res.data.EM);
+    if (data && data.EC === 0) {
+      toast.success(data.EM);
       handleClose();
     }
 
-    if (res.data && res.data.EC !== 0) {
-      toast.error(res.data.EM);
+    if (data && data.EC !== 0) {
+      toast.error(data.EM);
       handleClose();
     }
   };
